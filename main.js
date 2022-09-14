3ce15078759f39e394e4ff9f24e7e3e9c01c4d39
Jason Gagante <gagante_jason@yahoo.com>
2022-09-13 18:00:14 -0700

    added usopp and sanji

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const zoroBtn = document.querySelector(".btn-zoro");
 const zoroInfo = document.querySelector(".zoro-info");
+const namiBtn = document.querySelector(".btn-nami");
+const namiInfo = document.querySelector(".nami-info")
+const usoppBtn = document.querySelector(".btn-usopp");
+const usoppInfo = document.querySelector(".usopp-info")
+const sanjiBtn = document.querySelector(".btn-sanji");
+const sanjiInfo = document.querySelector(".sanji-info")
 
 zoroBtn.addEventListener('click', function() {
     if (zoroInfo.style.visibility === "hidden") {
@@ -8,4 +14,29 @@ zoroBtn.addEventListener('click', function() {
         zoroInfo.style.visibility = "hidden";
       }
 })
+
+namiBtn.addEventListener('click', function() {
+    if (namiInfo.style.visibility === "hidden") {
+        namiInfo.style.visibility = "visible";
+      } else {
+        namiInfo.style.visibility = "hidden";
+      }
+})
+
+usoppBtn.addEventListener('click', function() {
+    if (usoppInfo.style.visibility === "hidden") {
+        usoppInfo.style.visibility = "visible";
+      } else {
+        usoppInfo.style.visibility = "hidden";
+      }
+})
+
+sanjiBtn.addEventListener('click', function() {
+    if (sanjiInfo.style.visibility === "hidden") {
+        sanjiInfo.style.visibility = "visible";
+      } else {
+        sanjiInfo.style.visibility = "hidden";
+      }
+})
+
 
