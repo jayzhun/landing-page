dfcaabf36b4761c246044408146c62ce7c05ca55
Jason Gagante <gagante_jason@yahoo.com>
2022-09-13 19:37:25 -0700

    finished strawhat profiles

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ const usoppBtn = document.querySelector(".btn-usopp");
 const usoppInfo = document.querySelector(".usopp-info")
 const sanjiBtn = document.querySelector(".btn-sanji");
 const sanjiInfo = document.querySelector(".sanji-info")
+const chopperBtn = document.querySelector(".btn-chopper");
+const chopperInfo = document.querySelector(".chopper-info")
+const robinBtn = document.querySelector(".btn-robin");
+const robinInfo = document.querySelector(".robin-info")
+const frankyBtn = document.querySelector(".btn-franky");
+const frankyInfo = document.querySelector(".franky-info")
+const brookBtn = document.querySelector(".btn-brook");
+const brookInfo = document.querySelector(".brook-info")
+const jinbeBtn = document.querySelector(".btn-jinbe");
+const jinbeInfo = document.querySelector(".jinbe-info")
+
 
 zoroBtn.addEventListener('click', function() {
     if (zoroInfo.style.visibility === "hidden") {
@@ -38,5 +49,46 @@ sanjiBtn.addEventListener('click', function() {
         sanjiInfo.style.visibility = "hidden";
       }
 })
+
+chopperBtn.addEventListener('click', function() {
+    if (chopperInfo.style.visibility === "hidden") {
+        chopperInfo.style.visibility = "visible";
+      } else {
+        chopperInfo.style.visibility = "hidden";
+      }
+})
+
+robinBtn.addEventListener('click', function() {
+    if (robinInfo.style.visibility === "hidden") {
+        robinInfo.style.visibility = "visible";
+      } else {
+        robinInfo.style.visibility = "hidden";
+      }
+})
+
+frankyBtn.addEventListener('click', function() {
+    if (frankyInfo.style.visibility === "hidden") {
+        frankyInfo.style.visibility = "visible";
+      } else {
+        frankyInfo.style.visibility = "hidden";
+      }
+})
+
+brookBtn.addEventListener('click', function() {
+    if (brookInfo.style.visibility === "hidden") {
+        brookInfo.style.visibility = "visible";
+      } else {
+        brookInfo.style.visibility = "hidden";
+      }
+})
+
+jinbeBtn.addEventListener('click', function() {
+    if (jinbeInfo.style.visibility === "hidden") {
+        jinbeInfo.style.visibility = "visible";
+      } else {
+        jinbeInfo.style.visibility = "hidden";
+      }
+})
+
 
 
