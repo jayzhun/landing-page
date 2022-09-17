532e56a5d13d3713d8254f8671690f38d60ca0c1
Jason Gagante <gagante_jason@yahoo.com>
2022-09-17 15:37:56 -0700

    add dark/light mode

--- main.js
+++ main.js
@@ -1,94 +1,97 @@
 const zoroBtn = document.querySelector(".btn-zoro");
 const zoroInfo = document.querySelector(".zoro-info");
 const namiBtn = document.querySelector(".btn-nami");
-const namiInfo = document.querySelector(".nami-info")
+const namiInfo = document.querySelector(".nami-info");
 const usoppBtn = document.querySelector(".btn-usopp");
-const usoppInfo = document.querySelector(".usopp-info")
+const usoppInfo = document.querySelector(".usopp-info");
 const sanjiBtn = document.querySelector(".btn-sanji");
-const sanjiInfo = document.querySelector(".sanji-info")
+const sanjiInfo = document.querySelector(".sanji-info");
 const chopperBtn = document.querySelector(".btn-chopper");
-const chopperInfo = document.querySelector(".chopper-info")
+const chopperInfo = document.querySelector(".chopper-info");
 const robinBtn = document.querySelector(".btn-robin");
-const robinInfo = document.querySelector(".robin-info")
+const robinInfo = document.querySelector(".robin-info");
 const frankyBtn = document.querySelector(".btn-franky");
-const frankyInfo = document.querySelector(".franky-info")
+const frankyInfo = document.querySelector(".franky-info");
 const brookBtn = document.querySelector(".btn-brook");
-const brookInfo = document.querySelector(".brook-info")
+const brookInfo = document.querySelector(".brook-info");
 const jinbeBtn = document.querySelector(".btn-jinbe");
-const jinbeInfo = document.querySelector(".jinbe-info")
+const jinbeInfo = document.querySelector(".jinbe-info");
 
+zoroBtn.addEventListener("click", function () {
+  if (zoroInfo.style.visibility === "hidden") {
+    zoroInfo.style.visibility = "visible";
+  } else {
+    zoroInfo.style.visibility = "hidden";
+  }
+});
 
-zoroBtn.addEventListener('click', function() {
-    if (zoroInfo.style.visibility === "hidden") {
-        zoroInfo.style.visibility = "visible";
-      } else {
-        zoroInfo.style.visibility = "hidden";
-      }
-})
+namiBtn.addEventListener("click", function () {
+  if (namiInfo.style.visibility === "hidden") {
+    namiInfo.style.visibility = "visible";
+  } else {
+    namiInfo.style.visibility = "hidden";
+  }
+});
 
-namiBtn.addEventListener('click', function() {
-    if (namiInfo.style.visibility === "hidden") {
-        namiInfo.style.visibility = "visible";
-      } else {
-        namiInfo.style.visibility = "hidden";
-      }
-})
+usoppBtn.addEventListener("click", function () {
+  if (usoppInfo.style.visibility === "hidden") {
+    usoppInfo.style.visibility = "visible";
+  } else {
+    usoppInfo.style.visibility = "hidden";
+  }
+});
 
-usoppBtn.addEventListener('click', function() {
-    if (usoppInfo.style.visibility === "hidden") {
-        usoppInfo.style.visibility = "visible";
-      } else {
-        usoppInfo.style.visibility = "hidden";
-      }
-})
+sanjiBtn.addEventListener("click", function () {
+  if (sanjiInfo.style.visibility === "hidden") {
+    sanjiInfo.style.visibility = "visible";
+  } else {
+    sanjiInfo.style.visibility = "hidden";
+  }
+});
 
-sanjiBtn.addEventListener('click', function() {
-    if (sanjiInfo.style.visibility === "hidden") {
-        sanjiInfo.style.visibility = "visible";
-      } else {
-        sanjiInfo.style.visibility = "hidden";
-      }
-})
+chopperBtn.addEventListener("click", function () {
+  if (chopperInfo.style.visibility === "hidden") {
+    chopperInfo.style.visibility = "visible";
+  } else {
+    chopperInfo.style.visibility = "hidden";
+  }
+});
 
-chopperBtn.addEventListener('click', function() {
-    if (chopperInfo.style.visibility === "hidden") {
-        chopperInfo.style.visibility = "visible";
-      } else {
-        chopperInfo.style.visibility = "hidden";
-      }
-})
+robinBtn.addEventListener("click", function () {
+  if (robinInfo.style.visibility === "hidden") {
+    robinInfo.style.visibility = "visible";
+  } else {
+    robinInfo.style.visibility = "hidden";
+  }
+});
 
-robinBtn.addEventListener('click', function() {
-    if (robinInfo.style.visibility === "hidden") {
-        robinInfo.style.visibility = "visible";
-      } else {
-        robinInfo.style.visibility = "hidden";
-      }
-})
+frankyBtn.addEventListener("click", function () {
+  if (frankyInfo.style.visibility === "hidden") {
+    frankyInfo.style.visibility = "visible";
+  } else {
+    frankyInfo.style.visibility = "hidden";
+  }
+});
 
-frankyBtn.addEventListener('click', function() {
-    if (frankyInfo.style.visibility === "hidden") {
-        frankyInfo.style.visibility = "visible";
-      } else {
-        frankyInfo.style.visibility = "hidden";
-      }
-})
+brookBtn.addEventListener("click", function () {
+  if (brookInfo.style.visibility === "hidden") {
+    brookInfo.style.visibility = "visible";
+  } else {
+    brookInfo.style.visibility = "hidden";
+  }
+});
 
-brookBtn.addEventListener('click', function() {
-    if (brookInfo.style.visibility === "hidden") {
-        brookInfo.style.visibility = "visible";
-      } else {
-        brookInfo.style.visibility = "hidden";
-      }
-})
+jinbeBtn.addEventListener("click", function () {
+  if (jinbeInfo.style.visibility === "hidden") {
+    jinbeInfo.style.visibility = "visible";
+  } else {
+    jinbeInfo.style.visibility = "hidden";
+  }
+});
 
-jinbeBtn.addEventListener('click', function() {
-    if (jinbeInfo.style.visibility === "hidden") {
-        jinbeInfo.style.visibility = "visible";
-      } else {
-        jinbeInfo.style.visibility = "hidden";
-      }
-})
+const checkbox = document.querySelector("#checkbox");
 
-
-
+checkbox.addEventListener("change", () => {
+  // Toggle website theme
+  document.body.classList.toggle("dark");
+});
